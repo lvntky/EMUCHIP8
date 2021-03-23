e1e32ef7c9d05e4613709d5caa90d7b2d1d4d32c
Levent Kaya <42411502+lvntky@users.noreply.github.com>
2021-03-23 22:04:45 +0300

    config header added

--- include/chip8.h
+++ include/chip8.h
@@ -1,6 +1,7 @@
 #ifndef CHIP8_H
 #define CHIP8_H
 
+#include "config.h"
 struct chip8
 {
     /* data */
--- src/main.c
+++ src/main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include "D:\GitHub\EMUCHIP8\include\SDL2\SDL.h"
+#include "D:\GitHub\EMUCHIP8\include\chip8.h"
 
 int main(int argc, char** argv)
 {   
     SDL_Init(SDL_INIT_EVERYTHING); 
     SDL_Window* window = SDL_CreateWindow(
-        "CHIP-8",
+        CHIP8_WINDOW_TITLE,
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
-        640, 320, SDL_WINDOW_SHOWN
+        CHIP8_WIDTH * CHIP8_WINDOW_MULTIPLIER,
+        CHIP8_HEIGHT * CHIP8_WINDOW_MULTIPLIER, SDL_WINDOW_SHOWN
     );
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
