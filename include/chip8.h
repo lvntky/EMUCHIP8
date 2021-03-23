26049aa18584c4ea98782f80cc8d2e06e538cb19
Levent Kaya <42411502+lvntky@users.noreply.github.com>
2021-03-23 21:53:41 +0300

    include and configuration
    basix config and configuration changes.

---
+++ include/chip8.h
@@ -0,0 +1,10 @@
+#ifndef CHIP8_H
+#define CHIP8_H
+
+struct chip8
+{
+    /* data */
+};
+
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
+#include "D:\GitHub\EMUCHIP8\include\SDL2\SDL.h"
 
 int main(int argc, char** argv)
 {   
-    printf("Hello World!");
+    SDL_Init(SDL_INIT_EVERYTHING); 
+    SDL_Window* window = SDL_CreateWindow(
+        "CHIP-8",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640, 320, SDL_WINDOW_SHOWN
+    );
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
+    while (1){
+        SDL_Event event;
+        while (SDL_PollEvent(&event)){
+            if (event.type == SDL_QUIT){
+                goto out;
+            }
+            
+        }
+        
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+        SDL_Rect r;
+        r.x = 0;
+        r.y = 0;
+        r.h = 40;
+        r.w = 40;
+        SDL_RenderFillRect(renderer, &r);
+        SDL_RenderPresent(renderer);
+    }
+     
+out:
+    SDL_DestroyWindow(window);
     return 0;
 }
